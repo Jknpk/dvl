d570b65537030bffe1eeeaa50b3ff4f10d0311fe
Jannis <j.konopka@fz-juelich.de>
2016-05-25 13:05:52 +0200

    Test Stuff erweitert

--- src/client/testTaktik.java
+++ src/client/testTaktik.java
@@ -1,17 +1,23 @@
 package client;
 
+import java.util.List;
+
 import generated.AwaitMoveMessageType;
+import generated.BoardType;
+import generated.CardType;
 import generated.MoveMessageType;
-import generated.*;
+import generated.PositionType;
+import generated.TreasureType;
+import generated.TreasuresToGoType;
 
 public class testTaktik implements Taktik {
 
 	public MoveMessageType getMove(AwaitMoveMessageType awaitMoveMessage) {
 		BoardType bt = awaitMoveMessage.getBoard();
 		TreasureType tp = awaitMoveMessage.getTreasure();
-		awaitMoveMessage.getFoundTreasures();
-		awaitMoveMessage.getTreasuresToGo();
-
+		List<TreasureType> ltp = awaitMoveMessage.getFoundTreasures();
+		List<TreasuresToGoType> lttg = awaitMoveMessage.getTreasuresToGo();
+		
 		// Zu dem Ding müssen wir hin!
 		System.out.println("Zu diesem Schatz müssen wir unbedingt hin: "
 				+ tp.name());
@@ -23,23 +29,48 @@ public class testTaktik implements Taktik {
 //						+ bt.getForbidden().getRow());
 		// Hier sind die Öffnungen unserer Karte zu sehen
 		String s = "Neue Reinschiebkarte ist offen nach: ";
-		if (bt.getShiftCard().getOpenings().isTop())
-			s += "oben ";
-		if (bt.getShiftCard().getOpenings().isRight())
-			s += "rechts ";
-		if (bt.getShiftCard().getOpenings().isBottom())
-			s += "unten ";
-		if (bt.getShiftCard().getOpenings().isLeft())
-			s += "links ";
-		System.out.println(s);
+		if(bt.getShiftCard().getOpenings() != null){
+			if (bt.getShiftCard().getOpenings().isTop())
+				s += "oben ";
+			if (bt.getShiftCard().getOpenings().isRight())
+				s += "rechts ";
+			if (bt.getShiftCard().getOpenings().isBottom())
+				s += "unten ";
+			if (bt.getShiftCard().getOpenings().isLeft())
+				s += "links ";
+			System.out.println(s);
+		}
+		
 		s = "Spielfeld: \n";
 		for(int i = 0; i < bt.getRow().size(); i++){
 			for(int j = 0; j < bt.getRow().size(); j++){
-				s += bt.getRow().get(i).getCol().get(j).getTreasure().name() + " ";
+				if(bt.getRow().get(i).getCol().get(j).getTreasure() != null){
+					s += bt.getRow().get(i).getCol().get(j).getTreasure().name() + "\t";
+				}
+				else{
+					s += "\t";
+				}
+				
 			}
 			s+= "\n";
 		}
 		System.out.println(s);
+	
+		// Gibt die schon gefundenen Schätze aller Spieler aus 
+		for(int i = 0; i < ltp.size(); i++){
+			System.out.println(i+ ": " + ltp.get(i).name());
+		}
+		
+		
+		
+		// So viele Schätze müssen die einzelnen Spieler noch finden!
+		for(int i = 0; i < lttg.size(); i++){
+			System.out.println(i+ ": " + lttg.get(i).getTreasures());
+		}
+		
+		
+		
+		
 		
 		
 		PositionType newPinPos = new PositionType();
