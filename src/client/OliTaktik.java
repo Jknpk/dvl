5f162fa2f443cc9d55eca8676dee5440ead714b6
oliver <oliver@wlabs.de>
2016-06-01 12:50:27 +0200

    meine Taktik

---
+++ src/client/OliTaktik.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package client;
+
+import generated.AwaitMoveMessageType;
+import generated.MoveMessageType;
+
+/**
+ * @author oliver
+ *
+ */
+public class OliTaktik implements Taktik {
+
+	/* (non-Javadoc)
+	 * @see client.Taktik#getMove(generated.AwaitMoveMessageType)
+	 */
+	@Override
+	public MoveMessageType getMove(AwaitMoveMessageType awaitMoveMessage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
