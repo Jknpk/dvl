ac271df72ef1a0f020a7dae6a502f6910547e492
oliver <oliver@wlabs.de>
2016-06-01 14:11:06 +0200

    update taktik

--- src/client/OliTaktik.java
+++ src/client/OliTaktik.java
@@ -9,7 +9,9 @@ import generated.AwaitMoveMessageType;
 import generated.BoardType;
 import generated.BoardType.Row;
 import generated.CardType;
+import generated.CardType.Openings;
 import generated.MoveMessageType;
+import generated.PositionType;
 import generated.TreasureType;
 import generated.TreasuresToGoType;
 
@@ -23,6 +25,8 @@ public class OliTaktik implements Taktik {
 	private List<TreasureType> foundTreasures;
 	private TreasureType treasure;
 	private List<TreasuresToGoType> treasuresToGo;
+	private int needRow;
+	private int needColumn;
 
 	/*
 	 * (non-Javadoc)
@@ -36,25 +40,92 @@ public class OliTaktik implements Taktik {
 		foundTreasures = awaitMoveMessage.getFoundTreasures();
 		treasure = awaitMoveMessage.getTreasure();
 		treasuresToGo = awaitMoveMessage.getTreasuresToGo();
+
+		// print treasure to go
+		System.out.println("Treasure to go: " + treasure.name() + "\n");
+
+		// print board
+		printBoard();
+
+		// print shifted card
+		printShiftedCard();
+		
+		// TODO print pin position
+		
 		MoveMessageType moveMessage = new MoveMessageType();
-		System.out.println("Treasure to go: " + treasure.name());
 		// find treasure on board
-		boolean found = false;
+		findTreasurePosition();
+		System.out.println("searched card position\n\trow: " + needRow + " column: " + needColumn);
+		// is direct way to treasure
+		boolean directWay = isDirectWay();
+		if (directWay) {
+			PositionType position = new PositionType();
+			position.setRow(needColumn);
+			position.setCol(needColumn);
+			moveMessage.setNewPinPos(position);
+		}
+
+		return moveMessage;
+	}
+
+	private void printShiftedCard() {
+		System.out.println("shifted card: ");
+		StringBuilder sb = new StringBuilder();
+		CardType shiftCard = board.getShiftCard();
+		Openings openings = shiftCard.getOpenings();
+		sb.append("\topen to: ");
+		if (openings.isBottom()) {
+			sb.append("bottem ");
+		}
+		if (openings.isLeft()) {
+			sb.append("left ");
+		}
+		if (openings.isRight()) {
+			sb.append("right ");
+		}
+		if (openings.isTop()) {
+			sb.append("top ");
+		}
+		sb.append("\n");
+		System.out.println(sb.toString());
+	}
+
+	private void printBoard() {
+		System.out.println("Board: ");
+		List<Row> rows = board.getRow();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < rows.size(); i++) {
+			List<CardType> col = rows.get(i).getCol();
+			for (int j = 0; j < col.size(); j++) {
+				if (col.get(j).getTreasure() != null) {
+					sb.append(col.get(j).getTreasure().name() + "   \t");
+				} else {
+					sb.append("******   \t");
+				}
+			}
+			sb.append("\n");
+		}
+		sb.append("\n");
+		System.out.println(sb.toString());
+	}
+
+	private boolean isDirectWay() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private void findTreasurePosition() {
 		List<Row> rows = board.getRow();
 		for (int i = 0; i < rows.size(); i++) {
 			List<CardType> cols = rows.get(i).getCol();
 			for (int j = 0; j < cols.size(); j++) {
 				if (cols.get(j).getTreasure() != null && cols.get(j).getTreasure().name().equals(treasure.name())) {
-					System.out.println("searched card position\n\trow: " + i + " column: " + j);
-					found = true;
-					break;
+					needRow = i;
+					needColumn = j;
+					return;
 				}
 			}
-			if (found) {
-				break;
-			}
 		}
-		return moveMessage;
 	}
 
 }
